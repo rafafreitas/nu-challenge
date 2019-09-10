d08d999bb4c09e9a0ac9e844cb59cba4ba1bcb7d
Rafael Freitas <rafael.vasconcelos@outlook.com>
2019-09-10 12:59:42 -0300

    Ajustar auth controller .

--- src/Controller/Auth.js
+++ src/Controller/Auth.js
@@ -6,24 +6,15 @@ const AuthController = class Auth {
   static setClient(account){
 
     if (Object.keys(account).length === 0 && account.constructor !== Object) {
-      return {
-        status: 400,
-        message: 'account is not defined or not object'
-      }
+      return { status: 400, message: 'account is not defined or not object' }
     }
 
-    if (account.activeCard === null || account.activeCard === undefined || typeof account.activeCard === "boolean") {
-      return {
-        status: 400,
-        message: 'activeCard is not defined or not boolean'
-      }
+    if (account.activeCard === null || account.activeCard === undefined || typeof account.activeCard !== "boolean") {
+      return { status: 400, message: 'activeCard is not defined or not boolean' }
     }
 
     if (account.availableLimit === null || account.availableLimit === undefined || !Number.isInteger(account.availableLimit)) {
-      return {
-        status: 400,
-        message: 'availableLimit is not defined or not integer'
-      }
+      return { status: 400, message: 'availableLimit is not defined or not integer' }
     }
 
     if (Store.has('account')) {
