9b5b1d640c860a0054fca69429e17f5122b10603
Rafael Freitas <rafael.vasconcelos@outlook.com>
2019-09-10 19:28:32 -0300

    Ajustar envs.

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const express = require('express');
 const Routers = require('./src/Routes')
 const Store = require('./src/Config/Store')
 const app = express()
-
+const PORT = 3000
 Store.clear()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.listen(3000, () => {})
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
 app.use(Routers)
 
 
--- src/Config/Store.js
+++ src/Config/Store.js
@@ -1,3 +1,4 @@
-const Store = new (require('data-store'))('base', { cwd: './'});
+const DB = process.env.ENV_DB_BASE;
+const Store = new (require('data-store'))(DB, { cwd: './'});
 
 module.exports = Store;
