1b9eaaa6dc1440d348fc03c6e14c3d0677957833
Rafael Freitas <rafael.vasconcelos@outlook.com>
2019-09-11 00:31:18 -0300

    Ajustar Store e Routes

--- src/Config/Store.js
+++ src/Config/Store.js
@@ -1,4 +1,4 @@
-const DB = process.env.ENV_DB_BASE;
+const DB = process.env.NODE_ENV_DB_BASE;
 const Store = new (require('data-store'))(DB, { cwd: './'});
 
 module.exports = Store;
--- src/Routes/auth.js
+++ src/Routes/auth.js
@@ -4,10 +4,12 @@ const express = require('express');
 const app = express()
 
 app.post('/auth', (req, resp) => {
-
-  const obj = Controller.setClient(req.body.account)
-  resp.status(obj.status).send(obj.message);
-
+  try {
+    const obj = Controller.setClient(req.body.account)
+    resp.status(obj.status).send(obj.message);
+  } catch (e) {
+    resp.status(500).send(e.message);
+  }
 })
 
 module.exports = app
